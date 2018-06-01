663bacc4366e85301d0ce789e4510b8961c60e31
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2018-06-01 15:46:16 +0800

    #進度

---
+++ Assets/PlayMaker Custom Actions/setDOFFocusDistance.cs
@@ -0,0 +1,66 @@
+﻿// Custom Action by DumbGameDev
+// www.dumbgamedev.com
+
+using UnityEngine;
+using UnityEngine.PostProcessing.Utilities;
+using UnityEngine.PostProcessing;
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("Post Processing DOF")]
+	[Tooltip("Set Depth of Field Focus Distance Post Processing Effects.")]
+	public class setDOFFocusDistance : FsmStateAction
+	{
+
+		[RequiredField]
+		[CheckForComponent(typeof(PostProcessingController))]    
+		public FsmOwnerDefault gameObject;
+
+		public FsmFloat focusDistance;
+
+		public FsmBool everyFrame;
+
+		UnityEngine.PostProcessing.Utilities.PostProcessingController behavior;
+
+		public override void Reset()
+		{
+
+			focusDistance = null;
+			everyFrame = false;
+		}
+        
+		public override void OnEnter()
+		{
+
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			behavior = go.GetComponent<UnityEngine.PostProcessing.Utilities.PostProcessingController>();
+
+			if (!everyFrame.Value)
+			{
+				doPostProcess();
+				Finish();
+			}
+
+		}
+
+		public override void OnUpdate()
+		{
+			if (everyFrame.Value)
+			{
+				doPostProcess();
+			}
+		}
+			
+		void doPostProcess()
+		{
+
+			var go = Fsm.GetOwnerDefaultTarget (gameObject);
+			behavior = go.GetComponent<UnityEngine.PostProcessing.Utilities.PostProcessingController>();
+
+			behavior.depthOfField.focusDistance = focusDistance.Value;
+
+		}
+
+	}
+}
