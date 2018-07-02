5b9da51451fbe159c00d01d631f3113b7d620944
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2018-07-02 21:06:35 +0800

    #新增特效控制

---
+++ Assets/Project/Scene/EffectEnum.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class EffectEnum : MonoBehaviour {
+
+public enum EffectControl
+    {
+        AO,
+		SSRR,
+		BLOOM,
+		BLUR
+    }
+	
+
